2774fc54da5904e4e6db90b86a8185922a129ca4
Sumit Kumar Jha <88476958+sumit-kumar-jha1709@users.noreply.github.com>
2022-08-26 14:41:45 +0530

    Update Navbar.jsx

--- react-shopon-ui/src/components/Navbar.jsx
+++ react-shopon-ui/src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>ShopOn</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
